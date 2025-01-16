01434ebc6f761050de43f333dc482fa44b64844b
Huda Salman Luabi Al-Maliki <huda.luabi@qi.iq>
2025-01-16 14:17:42 +0300

    coin_flip.py

--- coin_flip.py
+++ coin_flip.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import pandas as pd 
 from random import choice
-
+st.image(r"C:\Users\huda.luabi\Pictures\Saved Pictures\coin.jpg")
 
 st.title("welcome here! ")
-st.text("this is a simple game \n so test out your like and enjoy ")
+st.subheader("this is a simple game \n so test out your like and enjoy ")
+name=st.text_input("what is the nameof the brave sodlier ?")
 def coin_flip():
   player=st.selectbox(options=["heads","tails"],label="pick your choice")
   system=choice(["heads","tails"])
   if player==system:
-    st.write("player wins")
+    st.write("player wins, you",name," are super lucky ")
   else:
-      st.write("player did not win")
-st.write(coin_flip())
-
+      st.write("player",name," did not win")
+      st.write("try again next time! :blue[*loser*]")
+st.text(coin_flip())
 
