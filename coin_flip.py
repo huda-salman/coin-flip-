77523b8ac4a2337a81323b9e4e60359ffb796934
Huda Salman Luabi Al-Maliki <huda.luabi@qi.iq>
2025-01-16 14:46:10 +0300

    coin_flip.py

--- coin_flip.py
+++ coin_flip.py
@@ -1,16 +1,9 @@
 import streamlit as st
 import pandas as pd 
 from random import choice
-page_bg_img = '''
-<style>
-body {
-background-image: url("C:\\Users\\huda.luabi\\Pictures\\Saved Pictures\\ocean.jpg");
-background-size: cover;
-}
-</style>
-'''
-st.image("C:\\Users\\huda.luabi\\Pictures\\Saved Pictures\\coinppint.jpg")
-st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+st.image("D:\\Git\\testing-git-app\\coinppint.jpg")
 st.title("welcome here! ")
 st.subheader("this is a simple game \n so test out your like and enjoy ")
 name=st.text_input("what is the :blue[name] of the brave sodlier ?")
