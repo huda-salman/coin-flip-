228385f37e001465c0332ee6a99b7484d1d2112a
Huda Salman Luabi Al-Maliki <huda.luabi@qi.iq>
2025-01-20 13:50:22 +0300

    rock_paper.py

---
+++ rock_paper.py
@@ -0,0 +1,24 @@
+import streamlit as st 
+from random import choice
+st.title("Rock Paper Scissors")
+st.subheader("choose your weapon here")
+st.button("play again:)")
+def THE_GAME():
+	system=choice(["rock","paper","scissors"])
+	player=st.radio("pick one",["rock","paper","scissors"])
+	if player==system:
+		st.write("Draw!, try again later:)")
+	elif player=="rock" and system=="scissors":
+		st.write("player wins!, you're a legend💪🏻")
+	elif player=="scissors" and system=="paper":
+		st.write("player wins!, you're a legend💪🏻")
+	elif player=="paper" and system=="rock":
+		st.write("player wins!, you're a legend💪🏻")
+	else:
+		st.write("player loses!, womp womp 🙂‍↕️")
+
+st.write(THE_GAME())
+
+
+
+
